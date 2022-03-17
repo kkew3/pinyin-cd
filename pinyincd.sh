f1494502183bf24e2c3cb0a758fc89e932d6808d
kkew3 <kps6326@hotmail.com>
2022-03-17 19:40:04 +0800

    Fixed issue where pycd not launched when not in project home.

--- pinyincd.sh
+++ pinyincd.sh
@@ -3,7 +3,7 @@ pycd() {
     local pycd_basedir="$(dirname "${BASH_SOURCE[0]:-${(%):-%x}}")"
 
     if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-        "$pycd_basedir/rt/bin/python" pinyincd.py --help
+        "$pycd_basedir/rt/bin/python" "$pycd_basedir/pinyincd.py" --help
     else
         selected="$("$pycd_basedir/rt/bin/python" pinyincd.py "$@" | fzf --exit-0 --select-1)"
         if [ -z "$selected" ]; then
